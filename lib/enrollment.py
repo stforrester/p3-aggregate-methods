b099bd71083e42dcc6ea5d807c46a4d81e5a6249
Stuart Forrester <stforrester@gmail.com>
2025-01-09 14:59:05 -0600

    completes the code-along mini-lesson

--- lib/enrollment.py
+++ lib/enrollment.py
@@ -3,6 +3,7 @@ class Student:
     def __init__(self, name):
         self.name = name
         self._enrollments = []
+        self._grades = {}
 
     def enroll(self, course):
         if isinstance(course, Course):
@@ -14,6 +15,16 @@ class Student:
 
     def get_enrollments(self):
         return self._enrollments.copy()
+    
+    def course_count(self):
+        return len(self._enrollments)
+    
+    def aggregate_average_grade(self):
+        total_grades = sum(self._grades.values())
+        num_courses = len(self._grades)
+        average_grade = total_grades / num_courses
+
+        return average_grade
 
 class Course:
     def __init__(self, title):
@@ -33,6 +44,14 @@ class Course:
 
 class Enrollment:
     all = []
+
+    @classmethod
+    def aggregate_enrollments_per_day(cls):
+        enrollment_count = {}
+        for enrollment in cls.all:
+            date = enrollment.get_enrollment_date().date()
+            enrollment_count[date] = enrollment_count.get(date, 0) +1
+        return enrollment_count
     
     def __init__(self, student, course):
         if isinstance(student, Student) and isinstance(course, Course):
